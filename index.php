b8768a12df6f3feaabe1f6ac98fa18bd61d8b2ba
Emylia <emyliarsd@gmail.com>
2020-02-25 11:32:59 +0800

    Update index.php
    test
    
    Co-Authored-By: narmadakumaran <narmadakumaran@users.noreply.github.com>

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     echo 'NRIC: <input type="text" name="nric"><br><br>';
 }
-
+//This is a comment
 ?>
 
 <input type="submit" value="Submit">
